d74f0792e03d7fbace2285dc10c30f92da5141b2
Alif Fathur <46101724+darkthur@users.noreply.github.com>
2019-03-05 12:39:48 +0700

    change

--- src/com/liquid/liquidlounge/recents/HAFRAppChooserDialog.java
+++ src/com/liquid/liquidlounge/recents/HAFRAppChooserDialog.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 The Liquid Remix Project
+ * Copyright (C) 2018 The FusionRemix ROM
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.liquid.liquidlounge.recents;
+package me.fusionremix.fusionlab.recents;
 
 import android.app.Dialog;
 import android.content.Context;
